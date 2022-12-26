41792c4300e37e9ed43885a87d918cb1b769d363
Sebastian Salinas <53806970+SebaSalinass@users.noreply.github.com>
2022-12-26 12:35:50 -0300

    Updated chart module

--- flask_chartjs/chart.py
+++ flask_chartjs/chart.py
@@ -1,8 +1,13 @@
-from typing import Literal, Optional, Union, Any, List, Dict
+import json
+
+from typing import Literal, Optional, Union, Any, Dict
 from dataclasses import dataclass, field
 
-from flask import Markup, render_template_string
-import json
+
+PaddingType = Union[int, Dict[Literal['left', 'top', 'right', 'bottom'], int]]
+AxisType = Literal['x', 'y']
+ChartType = Literal['area', 'bar', 'bubble', 'doughnut', 'pie', 'line', 'polarArea', 'radar', 'scatter']
+AlignmentType = Literal['center', 'left', 'right']
 
 
 @dataclass
@@ -15,7 +20,7 @@ class DataSet:
     stack: Optional[str] = None
     parsing: Optional[Union[bool, dict]] = None
     hidden: bool = False
-    data: List[Any] = field(default_factory=list)
+    data: list[Any] = field(default_factory=list)
     options: dict = field(default_factory=dict)
     background_color: Optional[str] = None
 
@@ -43,8 +48,8 @@ class DataSet:
 @dataclass
 class ChartData:
 
-    datasets: List[DataSet] = field(default_factory=list)
-    labels: List[str] = field(default_factory=list)
+    datasets: list[DataSet] = field(default_factory=list)
+    labels: list[str] = field(default_factory=list)
 
     def add_labels(self, *labels: str) -> None:
         for label in labels:
@@ -66,102 +71,51 @@ class ChartData:
 class Chart:
 
     id: str
-    type: Literal['area', 'bar', 'bubble', 'doughnut', 'line']
-    fallback: Optional[str] = None
+    type: ChartType
     data: ChartData = field(default_factory=ChartData)
-    plugins: List = field(default_factory=list, init=False)
-    options: Dict[str, Any] = field(default_factory=dict)
+    plugins: list = field(default_factory=list)
+    options: dict = field(default_factory=dict)
 
-    __tick_callbacks: Dict[str, str] = field(default_factory=dict, init=False)
+    _ticks_callbacks: Dict[str, str] = field(default_factory=dict, init=False)
+    _title: str = None
 
-    def __call__(self, title: Optional[Dict] = None,
-                 padding: Optional[Union[int, Dict]] = None,
-                 axis_title: Optional[Dict] = None,
-                 axis_tick_callback: Optional[Dict] = None) -> str:
-        """Renders a markup safe version of this chart.
+    def set_title(self, text: str, padding: PaddingType = None) -> None:
+        """Gives te chart a title.
 
-        Args:
-            title (Optional[Dict], optional): In template option to set `Chart title`, accepts a dict with the same arguments as `set_title`. Defaults to None.
-            padding (Optional[Dict], optional): In template option to set `Chart padding`, accepts a dict with the same arguments as `set_padding`. Defaults to None.
-            axis_title (Optional[Dict], optional): In template option to set `Axis title`, accepts a dict with the same arguments as `set_axis_title`. Defaults to None.
-            axis_tick_callback (Optional[Dict], optional): In template option to set `Axis tick callback`, accepts a dict with the same arguments as `set_title`. Defaults to None.
-
-        Returns:
-            str: An HTML safe string representing this chart.
+        :param text: The title to be set on the chart.
+        :type text: str
+        :param padding: The title padding an integer or {'left': 10} dict, defaults to None
+        :type padding: Union[int, Dict[str, int]], optional
         """
-        if title:
-            self.set_title(**title)
+        plugins = self.options.setdefault('plugins', dict())
+        title = plugins.setdefault('title', dict(display=True, text=text))
+        
         if padding:
-            self.set_padding(**padding)
-        if axis_title:
-            self.set_axis_title(**axis_title)
-        if axis_tick_callback:
-            for axis, callback in axis_tick_callback.items():
-                self.set_axis_tick_callback(axis, callback)
+            title['padding'] = padding
 
-        return Markup(render_template_string(self.chart_template_string(), chart=self))
-
-    def set_title(self, text: str, padding: Optional[Union[int, Dict[Literal['left', 'top', 'right', 'bottom'], int]]] = None) -> None:
-        """Sets the chart title.
-
-        Args:
-            title (str): Chart title.
-            padding (Optional[Union[int, Dict[Literal[&#39;left&#39;, &#39;top&#39;, &#39;right&#39;, &#39;bottom&#39;], int]]], optional): Title padding. Defaults to None.
-        """
-        plugins = self.options.setdefault('plugins', dict(
-            title=dict(
-                display=True,
-                text=text)))
-
-        if padding:
-            plugins['title']['padding'] = padding
-
-    def set_axis_title(self, axis: Literal['x', 'y'], text: str, align: Literal['start', 'center', 'end'] = 'center',
-                       weight: Optional[str] = None, size: int = 12) -> None:
-        """Sets title for given axis. could customize alignment and font weight and size.
-
-        Args:
-            axis (Literal[&#39;x&#39;, &#39;y&#39;]): The axis
-            title (str): The title
-            align (Literal[&#39;start&#39;, &#39;center&#39;, &#39;end&#39;], optional): Alignment options. Defaults to 'center'.
-            weight (Optional[str], optional): Font Weight. Defaults to None.
-            size (int, optional): Font size. Defaults to 12.
-        """
+    def set_axis_tick_callback(self, axis: AxisType, callback: str) -> None:
+        TOKEN = f'{axis}_tick_callback'
+        self._ticks_callbacks[TOKEN] = callback
+        
         scales = self.options.setdefault('scales', dict())
-        scales[axis] = dict(title=dict(
+        axis = scales.setdefault(axis, dict()) 
+        axis['ticks'] = dict(callback=TOKEN)
+
+    def set_axis_title(self, axis: AxisType,  text: str, align: AlignmentType = 'center', 
+                       weight: str = None, size: int = 12) -> None:
+        
+        scales = self.options.setdefault('scales', dict())
+        axis = scales.setdefault(axis, dict())
+        axis['title'] = dict(
             display=True,
-            text=text.capitalize(),
+            text=text,
             align=align,
-            font=dict(
-                weight=weight,
-                size=size
-            )
-        )
+            font=dict(weight=weight, size=size)
         )
 
-    def set_padding(self, padding: Union[int, Dict[Literal['left', 'top', 'right', 'bottom'], int]]) -> None:
-        """Set chart canvas padding.
-
-        Args:
-            padding (Union[int, Dict[Literal[&#39;left&#39;, &#39;top&#39;, &#39;right&#39;, &#39;bottom&#39;], int]]): The padding amount or padding dict object.
-        """
+    def set_padding(self, padding: PaddingType) -> None:
         layout = self.options.setdefault('layout', dict())
         layout['padding'] = padding
-
-    def set_axis_tick_callback(self, axis: Literal['x', 'y'], js_callback: str) -> None:
-        """Sets a tick callback for the given axys. Must be a Literal string JavasScript Function or name of a function returnning a string.
-
-        Args:
-            axis (Literal[&#39;x&#39;, &#39;y&#39;]): The axis.
-            js_callback (str): The function for the callback.
-        """
-        TOKEN = f'{axis.upper()}_CALLBACK_TOKEN'
-        self.__tick_callbacks[TOKEN] = js_callback
-
-        scales = self.options.setdefault('scales', dict())
-        axis_dict = scales.setdefault(axis, dict())
-        ticks = axis_dict.setdefault('ticks', dict())
-        ticks.setdefault('callback', TOKEN)
 
     def as_dict(self) -> Dict[str, Any]:
 
@@ -178,27 +132,9 @@ class Chart:
         return data_dict
 
     def to_json(self) -> str:
-        return_str = json.dumps(self.as_dict())
-
-        for token, js_callback in self.__tick_callbacks.items():
-            return_str = return_str.replace(f'"{token}"', Markup(js_callback))
+        return_str = json.dumps(self.as_dict(), indent=4)
+        for token, value in self._ticks_callbacks:
+            return_str.replace(f'\"{token}\"', value)
 
         return return_str
 
-    def chart_template_string(self) -> str:
-        return '''
-            {% set const_name = 'canvas_' + chart.id|replace('-', '_') %}
-            <canvas id="{{ chart.id }}">
-                <p>{{ chart.fallback or chart.type + ' ' + chart.id }}</p>
-            </canvas>
-            <script {% if csp_nonce %} nonce={{ csp_nonce() }} {% endif %}>
-            {% if htmx %}
-                var {{ const_name }} = htmx.find("#{{ chart.id }}")
-            {% else %}
-                var {{ const_name }} = document.querySelector("#{{ chart.id }}")
-            {% endif %}
-                new Chart({{ const_name }},
-                    {{ chart.to_json() | safe }}
-                    );
-            </script>
-            '''
